2f54a5b29b4a0ae9da015e50db3f1a13a1835582
willprovine <will.provine@kinkaid.org>
2022-08-19 10:30:16 -0500

    Initial commit

---
+++ JavaClass.java
@@ -0,0 +1,23 @@
+public class JavaClass {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.print("How are you\n I hope you are well");
+
+        int numDwarves; // instantiation
+        numDwarves = 7; // initialization
+
+        int numStooges = 3; // both instantiation and initialization
+
+        System.out.println("There are" +numDwarves+ " dwarves in my basement");
+        System.out.println("There were" +numStooges+ " Stooges until curly died");
+
+        numStooges= numStooges + 1;
+//        numStooges +=1;
+//        numStooges ++;
+        System.out.println("Now with Shemp there are " +numStooges+" stooges");
+
+    }
+
+}
